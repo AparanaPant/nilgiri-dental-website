5a06d3e9ecca067ee1bbc5c99781ceedebd348ef
Aparana <aparanapant@gmail.com>
2020-06-27 12:14:58 +0545

    blog slider

--- js/main.js
+++ js/main.js
@@ -34,3 +34,33 @@ function newSlide(n){
 function currentSlide(n){
   Carousel(slideNo = n);
 }
+//.............................blog slider...................//
+
+
+
+$(document).ready(function() {
+  $('#responsive').lightSlider({
+      item:3,
+      loop:false,
+      slideMove:2,
+      easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+      speed:600,
+      responsive : [
+          {
+              breakpoint:1440,
+              settings: {
+                  item:2,
+                  slideMove:1,
+                  slideMargin:6,
+                }
+          },
+          {
+              breakpoint:700,
+              settings: {
+                  item:1,
+                  slideMove:1
+                }
+          }
+      ]
+  });  
+});
