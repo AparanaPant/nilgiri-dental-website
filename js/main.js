7b8e05cf8be89150b77a853ed7e6b8091bf65ad7
Aparana <aparanapant@gmail.com>
2020-06-29 18:29:04 +0545

    nav

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 //----------------nav responsive view------------//
-$(document).ready(function() {
-        $('.menu-toggle').on('click',function() {
-        $('.nav-menu').toggleClass('show-nav');
-        });
-    
-      });
-
+(function($) { 
+  $(function() { 
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      $('.dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+  }); 
+})(jQuery);
 
 //=======================================================================//
 
